ac181cacee7cab2b003695a811e193ee402fe15f
Misha <michael.kharkovski@gmail.com>
2022-12-01 10:00:29 -0500

    removed placetype

--- Vysyt-app/src/app/global-vars.ts
+++ Vysyt-app/src/app/global-vars.ts
@@ -2,7 +2,7 @@ import { userInputs } from "./userInputs";
 
 export class GlobalVars {
   public static globalPlace: string;
-  public static globalPlaceType: string;
+  // public static globalPlaceType: string;
   public static globalMinVal: number;
   public static globalMaxVal: number;
 
