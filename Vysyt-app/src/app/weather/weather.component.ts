15beb165c878e16be7243bd408a0fa4111a85f5b
ZaneZepp22 <55850933+ZaneZepp22@users.noreply.github.com>
2022-12-03 13:26:20 -0500

    Update weather.component.ts

--- Vysyt-app/src/app/weather/weather.component.ts
+++ Vysyt-app/src/app/weather/weather.component.ts
@@ -14,10 +14,9 @@ import { Subscription } from 'rxjs';
 
 
 export class WeatherComponent {
-  // city = 'Pittsburgh';
-  // lat = '40.44';
-  // lon = '79.99';
-  city: string;
+  city = '';
+  lat = '';
+  lon = '';
   current_temp = ' degrees F';
   high_temp = ' degrees F';
   low_temp = ' degrees F';
@@ -27,29 +26,25 @@ export class WeatherComponent {
 
   constructor(private api: GetApiService, private sharedService: SharedService) {
     this.clickEventsubscription = this.sharedService.getClickEvent().subscribe(() => {
-      this.showWeatherData();
+      this.getApiData();
     })
   }
+
   ngOnInit() {
-    // this.api.getWeather().subscribe((data) => {
-    //   console.warn("get weather data", data);
-    //   this.current_temp = data['main']['temp'] + ' degrees F';
-    //   this.high_temp = data['main']['temp_max'] + ' degrees F';
-    //   this.low_temp = data['main']['temp_min'] + ' degrees F';
-    //   this.weather = data['weather'][0]['main'];
-    //   this.weather_description = data['weather'][0]['description'];
-    // })
   }
-  showWeatherData() {
-    this.city = GlobalVars.globalPlace;
-      this.api.getWeather().subscribe((data) => {
-        console.warn("get weather data", data);
+
+  getApiData() {
+    this.api.getWeather().subscribe((data) => {
+        console.log("get weather data", data);
+        this.city = GlobalVars.globalPlace;
+        this.lat = data['coord']['lat'];
+        this.lon = data['coord']['lon'];
         this.current_temp = data['main']['temp'] + ' degrees F';
         this.high_temp = data['main']['temp_max'] + ' degrees F';
         this.low_temp = data['main']['temp_min'] + ' degrees F';
         this.weather = data['weather'][0]['main'];
         this.weather_description = data['weather'][0]['description'];
-      })
+    })
   }
 
 }
