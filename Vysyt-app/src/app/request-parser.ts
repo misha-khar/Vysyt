3e35a994d07504c362b4274ed5ce81e220a6c0bf
JCP97-2024 <79323001+JCP97-2024@users.noreply.github.com>
2022-12-03 13:31:51 -0500

    Update request-parser.ts

--- Vysyt-app/src/app/request-parser.ts
+++ Vysyt-app/src/app/request-parser.ts
@@ -1,67 +1,6 @@
 import './request';
 import { request } from './request';
 
-
-let map: google.maps.Map;
-let service: google.maps.places.PlacesService;
-let infowindow: google.maps.InfoWindow;
-
-function initMap(): void {
-  const sydney = new google.maps.LatLng(-33.867, 151.195);
-
-  infowindow = new google.maps.InfoWindow();
-
-  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    center: sydney,
-    zoom: 15,
-  });
-
-  const request = {
-    query: "Museum of Contemporary Art Australia",
-    fields: ["name", "geometry"],
-  };
-
-  service = new google.maps.places.PlacesService(map);
-
-  service.findPlaceFromQuery(
-    request,
-    (
-      results: google.maps.places.PlaceResult[] | null,
-      status: google.maps.places.PlacesServiceStatus
-    ) => {
-      if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-        for (let i = 0; i < results.length; i++) {
-          createMarker(results[i]);
-        }
-
-        map.setCenter(results[0].geometry!.location!);
-      }
-    }
-  );
-}
-
-function createMarker(place: google.maps.places.PlaceResult) {
-  if (!place.geometry || !place.geometry.location) return;
-
-  const marker = new google.maps.Marker({
-    map,
-    position: place.geometry.location,
-  });
-
-  google.maps.event.addListener(marker, "click", () => {
-    infowindow.setContent(place.name || "");
-    infowindow.open(map);
-  });
-}
-
-declare global {
-  interface Window {
-    initMap: () => void;
-  }
-}
-window.initMap = initMap;
-export {};
-
 const pittsburgh = new google.maps.LatLng(40.441, -80.0);
 
 export class RequestParser {
@@ -69,7 +8,7 @@ export class RequestParser {
     private _service: google.maps.places.PlacesService;
     private _infowindow: google.maps.InfoWindow;
     private _request: request;
-    private _results = google.maps.places.PlaceResult;
+    private _results: google.maps.places.PlaceResult[];
 
     constructor() {
         this._infowindow = new google.maps.InfoWindow();
@@ -105,20 +44,18 @@ export class RequestParser {
 
     public sendRequest(){
       if (this.getQuery() === "") {return false;}
-      var r = {query:this.getQuery(), fields:this.getFields()}
+      var r = {query:this.getQuery(), fields:this._request.fields}
 
-      service.findPlaceFromQuery(
+      this._service.findPlaceFromQuery(
         r,
         (
           results: google.maps.places.PlaceResult[] | null,
           status: google.maps.places.PlacesServiceStatus
         ) => {
           if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-            for (let i = 0; i < results.length; i++) {
-              createMarker(results[i]);
-            }
-    
-            map.setCenter(results[0].geometry!.location!);
+            this._results.push(results[0]);
+            this._createMarker(results[0]);
+            this._map.setCenter(results[0].geometry!.location!);
           }
         }
       );
@@ -126,17 +63,17 @@ export class RequestParser {
       return true
     }
 
-    public createMarker(place: google.maps.places.PlaceResult) {
+    private _createMarker(place: google.maps.places.PlaceResult) {
       if (!place.geometry || !place.geometry.location) return;
     
       const marker = new google.maps.Marker({
-        map,
+        map: this._map,
         position: place.geometry.location,
       });
     
       google.maps.event.addListener(marker, "click", () => {
-        infowindow.setContent(place.name || "");
-        infowindow.open(map);
+        this._infowindow.setContent(place.name || "");
+        this._infowindow.open(this._map);
       });
     }
 }
