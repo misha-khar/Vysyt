d819b4da53e10b9bd8d4b477e5a4cfdd09fdc69c
Misha <michael.kharkovski@gmail.com>
2022-12-03 19:43:21 -0500

    Revert "minor"
    This reverts commit c7c5ed341c2b004e6749a732b91e5e8819168946.

--- Vysyt-app/src/app/map-display/map-display.component.ts
+++ Vysyt-app/src/app/map-display/map-display.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { RequestParser } from '../request-parser';
-import { GlobalVars } from '../global-vars';
-import { SharedService } from '../shared.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-map-display',
@@ -11,18 +8,7 @@ import { Subscription } from 'rxjs';
 })
 export class MapDisplayComponent implements OnInit {
 
-  clickEventsubscription: Subscription;
-
-  constructor(private sharedService: SharedService) {
-    this.clickEventsubscription = this.sharedService.getClickEvent().subscribe(() => {
-      this.showMap();
-    })
-  }
-
-  showMap(){
-
-
-  }
+  constructor() { }
 
   ngOnInit(): void {
     var r = new RequestParser();
