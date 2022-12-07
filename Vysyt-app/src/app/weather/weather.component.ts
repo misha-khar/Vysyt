f9be19a75cbde156783ecadbc13ab0e83b7c1c1f
Misha <michael.kharkovski@gmail.com>
2022-12-07 14:41:02 -0500

    Update weather.component.ts

--- Vysyt-app/src/app/weather/weather.component.ts
+++ Vysyt-app/src/app/weather/weather.component.ts
@@ -14,14 +14,7 @@ import { Subscription } from 'rxjs';
 
 
 export class WeatherComponent {
-//<<<<<<< main
-  // city = 'Pittsburgh';
-  //lat: number;
-  //long: number;
-  //place: string;
-  //current_temp = '';
-  //high_temp = '';
-  //low_temp = '';
+
   city = '(City)';
   lat = '';
   lon = '';
@@ -35,11 +28,7 @@ export class WeatherComponent {
 
   constructor(private api: GetApiService, private sharedService: SharedService) {
     this.clickEventsubscription = this.sharedService.getClickEvent().subscribe(() => {
-//<<<<<<< main
-      // this.getLatLong();
-      // this.wait(1000);
-      //this.showWeatherData();
-      // this.showWeatherData();
+
       this.getApiData();
     })
   }
@@ -49,40 +38,6 @@ export class WeatherComponent {
     this.getApiData();
   }
 
-//<<<<<<< main
-  //getLatLong() {
-    // this.wait(1000);
-    //this.place = GlobalVars.globalPlace;
-    //this.api.getLatLong(this.place).subscribe((data) => {
-      //console.warn("get coord data", data);
-      //GlobalVars.globalLat = data[0]['lat'];
-      //GlobalVars.globalLon = data[0]['lon'];
-      //console.log("lat: " + GlobalVars.globalLat + "    long: " + GlobalVars.globalLon);
-
-    //})
-    // this.wait(1000);
-  //}
-
-  //showWeatherData() {
-    //this.getLatLong();
-    //this.place = GlobalVars.globalPlace;
-    //this.api.getWeather().subscribe((data) => {
-      //console.warn("get weather data", data);
-      //this.current_temp = data['main']['temp'] + '° F';
-      //this.high_temp = data['main']['temp_max'] + '° F';
-      //this.low_temp = data['main']['temp_min'] + '° F';
-      //this.weather = data['weather'][0]['main'];
-      //this.weather_description = data['weather'][0]['description'];
-    //})
-  //}
-
-  //wait = (ms) => {
-    //const start = Date.now();
-    //let now = start;
-    //while (now - start < ms) {
-      //now = Date.now();
-    //}
-//=======
   getApiData() {
     this.api.getWeather().subscribe((data) => {
         console.log("get weather data", data);
